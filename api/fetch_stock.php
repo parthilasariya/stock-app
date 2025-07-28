f74306b4502e7d252917a47a124b21cbfe5fe942
Parth Ilasariya <parthilasariya@Parths-MacBook-Pro.local>
2025-07-27 23:18:13 -0300

    dbchange,fetchcode

--- api/fetch_stock.php
+++ api/fetch_stock.php
@@ -1,12 +1,27 @@
+<!-- A backend PHP script that acts as a bridge between frontend and the public stock data API (Alpha Vantage) -->
+
 <?php 
 require_once '..config/db.php'; // Database configuration file
 
 $symbol = 'RCI-B.TO'; // Toronto Stock Exchange stock symbol for Rogers Communications
 $apiKey = getenv('ALPHA_VANTAGE_API_KEY'); //Key is stored in .env file
 
-# Fetch stock data from Alpha Vantage API, I have recalled above var
-$url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=$symbol&interval=1min&apikey=$apiKey";
-$data = file_get_contents($url);
-$json = json_decode($data, true);
+# Fetch stock data from Alpha Vantage API asking 5min interval, I have recalled above var
+$url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=$symbol&interval=5min&apikey=$apiKey";
+
+$response = file_get_contents($url); // Sends HTTP req to the API and gets the JSON response
+$data = json_decode($response, true); // Decode JSON response
+
+// error handling for API response
+# !$data checks if the response is null
+if (!$data || !isset($data['Time Series (5min)'])) {
+    echo json_encode(['error' => 'failed to fetch stock data']);
+    exit;
+}
+
+$timeseries = $data['TIme Series (5min)'];
+$timestamps = array_keys($timeseries);
+
+
 
 ?>
--- config/db.php
+++ config/db.php
@@ -1,8 +1,21 @@
 <?php
+/**
+ * Database Configuration
+ * PDO connection setup for MySQL database
+ */
+
 $host='localhost';
 $dbname='stock_app';
 $user='root';
 $pass='';
-$charset='utf8mb4';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Database connection failed:']); // Helps for client-side error
+    exit;
+}
 ?>
